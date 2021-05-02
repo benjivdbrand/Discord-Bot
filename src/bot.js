362566a9c0ac52b00784f9cee83e60b4bf07c498
dcanlon <delorean.canlon@gmail.com>
2021-05-02 19:14:40 +0200

    ADD: various commands for raising hands
    Added help, question/hand up, hand down, list and clear

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var questionList = [];
 
 client.on('ready', readyDiscord);
 client.on('message', receiveMessage);
@@ -9,11 +10,65 @@ function readyDiscord() {
 }
 
 function receiveMessage(msg) {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+    if (msg.author.bot) return
+
+    const prefix = "!"
+
+    //List of possible commands
+    if (msg.content === prefix + 'help') {
+        msg.channel.send('Hello there! These are the commands that I support:');
+        msg.channel.send('```![question/hand up/hand down/next/pick < name >/list/clear]```');
     }
 
-    if (msg.content === 'join') {
+    //Commands for raising your hand
+    if (msg.content === prefix + 'question' || msg.content === prefix + 'hand up' ) {
+        //Check if the person has not raised their hand yet
+        if ( questionList.includes(msg.member.user.tag) ) {
+            msg.reply('hand is already raised. Please wait for your turn')
+        } else {
+            //If not raised, add them to the list of students with questions
+            questionList.push(msg.member.user.tag)
+            msg.channel.send("Hello " + msg.member.user.tag + '. Your hand is now raised')
+        }
+    }
+
+    //Commands for lowering your hand
+    if (msg.content === prefix + 'hand down') {
+        for (var i = 0; i < questionList.length; i++) {
+            if (questionList[i] === msg.member.user.tag) {
+                questionList.splice(i, 1)
+            }
+        }
+        msg.reply('your hand has been lowered.')
+    }
+
+    //Commands for showing the list
+    if (msg.content === prefix + 'list') {
+        let items = questionList.map((item, idx) =>
+            `${idx + 1}. Name: ${questionList[idx]}`);
+        if(items.length > 0) {
+            msg.channel.send(createInfoEmbed('Currently holding up their hands:', items.join('\n\n')));
+        } else {
+            msg.channel.send(createInfoEmbed(`There are no more raised hands.`, 'Use ```!question``` or ```!hand up``` to add items.'));
+        }
+    }
+
+    //Commands for clearing the list
+    if (msg.content === prefix + 'clear') {
+        if (questionList.length < 1) {
+            msg.channel.send('List is already empty. Use ```!question``` or ```!hand up``` to add items.')
+        } else {
+            //Reset array of people with questions
+            questionList = []
+            msg.channel.send('List is empty again.')
+        }
+    }
+
+    if (msg.content === prefix + 'helleuw') {
+        msg.reply('hello there!');
+    }
+
+    if (msg.content === prefix + 'join') {
         msg.member.voice.channel.join().then(connection => {
             msg.reply('I joined the voice channel!');
         }).catch(e => {
@@ -21,9 +76,18 @@ function receiveMessage(msg) {
         });
     }
 
-    if (msg.content === 'leave') {
+    if (msg.content === prefix + 'leave') {
         msg.guild.me.voice.channel.leave();
     }
 }
 
-client.login('ODM1OTc1NTgwODMwMDA3MzM2.YIXQ7g.it5gRU5Sv1zvPZ89MbM4NoOSIac');
+function createInfoEmbed(title, message) {
+    return new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(title)
+        .setDescription(message);
+}
+
+client.login("ODM3MDcyNzI5MTkwNDMyODA4.YInOug.2LVk6-t-buRSF-xI35juRuvGREU")
+// Original token of Professor Doctor Heinz
+// client.login("ODM1OTc1NTgwODMwMDA3MzM2.YIXQ7g.it5gRU5Sv1zvPZ89MbM4NoOSIac");
